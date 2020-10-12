b472f02e9fe78b490328f4ff1c3b821eb815a507
joelsutton <joel.sutton@myfoodbag.co.nz>
2020-10-12 16:37:39 +1300

    Better jest linting Build output is maintained by webpack

--- src/test/jest.config.js
+++ src/test/jest.config.js
@@ -1,7 +1,6 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-// eslint-disable-next-line no-undef
 module.exports = {
     moduleDirectories: ["node_modules"]
 };
